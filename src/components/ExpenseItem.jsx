268a7159b91a4a6243565366e824416eeb3e2218
SohamAmare <sohamamare07@gmail.com>
2025-04-18 21:05:36 +0530

    effect again fixed

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-const ExpenseItem = ({ expense }) => {
+const ExpenseItem = ({ expense, fetchMethod }) => {
   const handleDelete = async () => {
     try {
       await axios.delete(
